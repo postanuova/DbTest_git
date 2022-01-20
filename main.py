d885a8b11c84a0eab7609d29debc97cd2fede7d1
chris <postanuova@gmail.com>
2022-01-20 23:40:07 +0000

    primo

---
+++ main.py
@@ -0,0 +1,40 @@
+import mysql.connector
+
+
+def open_connection(user, password, database):
+    connection = mysql.connector.connect(user=user, password=password, host='localhost', database=database)
+    return connection
+
+
+def close_connection(connection):
+    connection.close()
+    print("connection closed")
+
+
+def do_query(connection, query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.commit()
+        return result
+
+
+# main
+try:
+    # aprire la connessione
+    connection = open_connection("root", "root", "sneakers")
+    # eseguire la query
+    query = """select brand,model,buyer_region from orders limit 1000"""
+    # query = """insert into delete_me values ("christo")"""
+    result = do_query(connection, query)
+    # visualizzare il resultset
+    for row in result:
+        print(row[1])
+
+# chiudere la connessione
+
+except mysql.connector.Error as e:
+    print(e)
+
+finally:
+    close_connection(connection)
